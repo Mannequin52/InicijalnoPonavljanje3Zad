0d3861a4f99b8fed7f6424d9e9b00631051d31cf
Mannequin52 <92926451+Mannequin52@users.noreply.github.com>
2023-09-11 10:46:30 +0200

    Add files via upload

---
+++ Inicijalni3_4D/Inicijalni3_4D/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalni3_4D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Napišite program u kojem ćete deklarirati varijable a i b tipa int, 
+             * c i d tipa double, dodijelite im vrijednosti 10, 7, 12.5 i 16.7, 
+             * te izračunajte prosječnu vrijednost.*/
+
+            int a=10, b=7;
+            double c=12.5, d=16.7;
+
+            double prosijek = 0;
+
+            prosijek = (a + b + c + d) / 4;
+            Console.WriteLine(prosijek);
+            Console.ReadKey();
+
+        }
+    }
+}
